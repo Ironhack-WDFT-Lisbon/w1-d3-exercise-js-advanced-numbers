7312218b9970a436726bf890cd69ad808b8e8458
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-11 22:30:52 +0100

    update after class

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Ready to go! 🚀");
+
+// Exercise 1
+const randomNum = Math.random() * 100;
+const roundedValue = Math.round(randomNum);
+const toFixedDecimals = Number(randomNum.toFixed(2));
+
+// Exercise 2
+const maxValue = Math.max(2, 8, -10, 0, -4, -7, 9);
+const minValue = Math.min(2, 8, -10, 0, -4, -7, 9);
